4325c9e08047ed3fbaf75e286df0774097e060b8
RubinVelcani <rubin.velcani@gmail.com>
2021-06-11 16:14:57 +0200

    'endpoint'

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -19,7 +19,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('')
     const [messages, setMessages] = useState([])
 
-    const ENDPOINT = 'localhost:5000'
+    const ENDPOINT = 'https://nodeiy-chat.herokuapp.com/'
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search.slice(1))
